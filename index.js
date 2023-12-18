3ae2b40bb37c880a46a707a11e3ed14aca1e472f
johnbarsigo <john.barsigo@student.moringaschool.com>
2023-12-18 17:56:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+let num3 = 32
+let num4 = 7
 
-// Write your code here
+let multiply = num1 * num2
+var random = Math.floor(Math.random() * 10) + 1
+let mod = num3 % num4
+let max = Math.max(12, 2, 7, 20, 3)
+
+//console.log( random )
